5d14efee97bb512f0ea305739597bf49838330f6
Aria Khodarahmi <ariakh25@gmail.com>
2021-07-15 00:11:09 +0430

    add recusiveMethod.js totalNumber

--- js/recursiveMethod.js
+++ js/recursiveMethod.js
@@ -1,14 +1,33 @@
 // Pow recursive Method
-function powMethod(number, pownum) {
+// function powMethod(number, pownum) {
 
-    if (pownum === 1) {
-        return number;
-    } else if (pownum === 0) {
-        return 1;
+//     if (pownum === 1) {
+//         return number;
+//     } else if (pownum === 0) {
+//         return 1;
+//     }
+
+//     return number * powMethod(number, pownum - 1);
+// }
+
+
+
+// console.log(powMethod(2, 5));
+
+
+
+
+// Total Number less than it
+function total(number) {
+    let numbers = [];
+    for (let i = 0; i <= number; i++) {
+        numbers.push(i);
+
     }
+    let sumNumber = numbers.reduce((sum, num) => sum + num, 0);
 
-    return number * powMethod(number, pownum - 1);
+    return sumNumber;
 }
 
 
-console.log(powMethod(2, 5));
+console.log(total(7));
